814a97abc7bae4ec3a078dc4cb2134d0bbb44a0c
alok <alok@lacework.net>
2020-03-03 01:31:08 +0000

    ARM64 build RAIN-11251

--- pcap.go
+++ pcap.go
@@ -15,10 +15,10 @@ void *__memcpy_glibc_2_2_5(void *, const void *, size_t);
 
 // We use memmove rather than memcpy here since it is safer
 // for overlapping memory regions.
-asm(".symver __memcpy_glibc_2_2_5, memmove@GLIBC_2.2.5");
+//asm(".symver __memcpy_glibc_2_2_5, memmove@GLIBC_2.2.5");
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
-    return __memcpy_glibc_2_2_5(dest, src, n);
+	return memcpy(dest, src, n);
 }
 #define MAX_PACKETS     10
 #define PCAP_DISPATCH_OVERFLOW 5
